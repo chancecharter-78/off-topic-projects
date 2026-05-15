76ba4185b972252e5cb6728abf9a8badcc7b4719
chancecharter-78 <chancecharter78@gmail.com>
2026-05-15 12:35:33 +0800

    Initial Draft of Main Src

---
+++ MainSrc.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileWriter;
+
+public class MainSrc 
+{
+  public static void main(String[] args)
+  {
+    Scanner in = new Scanner(System.in);
+
+    System.out.println("Welcome to my list of random trail-of-thought projects!")
+  }
+}
